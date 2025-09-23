a4d1fd19962202a6ad2eef8fd351729695c5a536
7vtr0x7 <vtrox7@gamil.com>
2025-09-23 17:11:03 +0530

    company model

---
+++ backend/src/models/company.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const CompanySchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    industry: String,
+    location: {
+      city: String,
+      state: String,
+      country: String,
+    },
+    employees: Number,
+    foundedYear: Number,
+    description: String,
+  },
+  { timestamps: true }
+);
+CompanySchema.index({ name: "text", description: "text", industry: "text" });
+
+export const company = mongoose.model("Company", CompanySchema);
